864233617c570150083acb97c49cacae43732d48
Micael Tahintsoa <mtahintsoa@gmail.com>
2026-04-23 12:40:44 +0300

    url

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['example.antonymica.site'],
+    allowedHosts: ['example.antonymica.site', 'very-simple.duckdns.org'],
   },
   plugins: [react(), tailwindcss()],
 });
