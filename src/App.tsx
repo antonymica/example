79efa38d3644ca663b1b014a479f194b6683911d
Micael Tahintsoa <mtahintsoa@gmail.com>
2026-04-23 11:19:38 +0300

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold ">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
